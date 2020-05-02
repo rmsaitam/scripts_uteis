41d93ea2789f91185d34d1e9a1a89ea3577c9ec0
Reginaldo <root.saitam@gmail.com>
2020-05-02 18:06:09 -0300

    sequencia P.G

---
+++ sequencia_pg/js/functions.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {
+    $("#btn_gerar").click(function(){
+
+        var a1 = $("#a1").val();
+        var q  = $("#q").val();
+        var n  = $("#n").val();
+
+        var seq = ' ';
+        
+        if(a1 == '' || q == '' || n == '') {
+            $("#mensagem").html("<b> <font color='red'>É obrigatório preencher todos os campos.</b>");
+            setTimeout(function() {
+                $("#mensagem").html("");
+            }, 5000);
+        }
+        else if(q == 0 || q == 1) {
+            $("#mensagem").html("<b> <font color='red'>A razão da P.G deve ser maior que 0 e diferente de 1</b>");
+            setTimeout(function() {
+                $("#mensagem").html("");
+            }, 5000);    
+        }
+        else if(n <= 0) {
+            $("#mensagem").html("<b> <font color='red'>O número de termos da P.G deve ser maior que 0</b>");
+            setTimeout(function() {
+                $("#mensagem").html("");
+            }, 5000);
+        }
+        else {
+             //an = a1*(q^(n-1))
+             an = parseFloat(a1) * parseFloat(Math.pow(q, parseInt((n-1))));
+             //Sn = a1*(q^n - 1) / (q-1)
+             Sn =  (parseFloat(a1) * parseFloat(Math.pow(q, n) -1) ) / parseFloat((q-1));
+    
+            for(var i=a1; i<=an; i=i*q) {
+                seq = seq +i+ ' ';
+            }
+        
+            $("#sequencia").html(seq);
+            $("#resultado").html("an = " +an+ " Sn = " +Sn);
+        }
+
+       
+    });
+});
